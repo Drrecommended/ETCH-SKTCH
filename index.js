cde7c42be481c2eaab5307b217d7e62c95e59ecd
Tyler French <tylerfrench89@gmail.com>
2022-05-27 20:45:55 -0700

    changed layout

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+const openModalBtn = document.getElementById("modal-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const overlay = document.getElementById("overlay");
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".reset-btn");
 const penColor = document.querySelector(".input-color");
-const gridInput = document.querySelector("#grid-input")
-
+const gridInput = document.getElementById("grid-input");
 
 let isDrawing = false;
 let enableDeleteBtn = false;
-let COLOR_CHOICE = "black"
-let GRID_SIZE = 100
+let COLOR_CHOICE = "black";
+let GRID_SIZE = 16;
 
+//0pen the settings modal
+const openModal = (modal) => {
+  if (modal == null) return;
+  modal.classList.add("active-modal");
+  overlay.classList.add("active-overlay");
+};
 
+//close the settings modal
+const closeModal = (modal) => {
+  if (modal == null) return;
+  modal.classList.remove("active-modal");
+  overlay.classList.remove("active-overlay");
+};
 
 //function to create custom grid
 const createGrid = (rows, cols) => {
@@ -27,9 +41,9 @@ const createGrid = (rows, cols) => {
 };
 
 const setGridSize = (e) => {
-  GRID_SIZE = e.target.value
-
-}
+  GRID_SIZE = e.target.value;
+  createGrid(GRID_SIZE, GRID_SIZE)
+};
 
 const enableDraw = (e) => {
   isDrawing = true;
@@ -45,10 +59,9 @@ const togglePen = (e) => {
   }
   // if (e.target.classList.contains("active")) return;
   e.target.style.cursor = "crosshair";
-  e.target.classList.add('active')
-  if(e.target.classList.contains('active')) {
-    
-    e.target.style.backgroundColor = COLOR_CHOICE
+  e.target.classList.add("active");
+  if (e.target.classList.contains("active")) {
+    e.target.style.backgroundColor = COLOR_CHOICE;
   }
 };
 
@@ -60,19 +73,37 @@ const clearGrid = () => {
   const cells = document.querySelectorAll(".grid-item");
   for (let cell of cells) {
     if (cell.classList.contains("active")) {
-      cell.classList.remove('active')
-      cell.style.backgroundColor = 'white'
+      cell.classList.remove("active");
+      cell.style.backgroundColor = "white";
     }
   }
-  gridInput.value = GRID_SIZE
-}
+  gridInput.value = GRID_SIZE;
+};
 
 const setColor = (e) => {
-  COLOR_CHOICE = e.target.value
-}
+  COLOR_CHOICE = e.target.value;
+};
 
 createGrid(GRID_SIZE, GRID_SIZE);
 
+openModalBtn.addEventListener("click", () => {
+  const modal = document.getElementById("modal");
+  openModal(modal);
+});
+
+closeModalBtn.addEventListener("click", () => {
+  const modal = closeModalBtn.closest('.modal')
+  closeModal(modal)
+})
+
+overlay.addEventListener('click', () => {
+  const modal = document.querySelector('.modal.active-modal')
+  closeModal(modal)
+})
+
 resetBtn.addEventListener("click", clearGrid);
-penColor.addEventListener("change", setColor)
-gridInput.addEventListener("change", setGridSize)
+penColor.addEventListener("change", setColor);
+gridInput.addEventListener("change", setGridSize);
+gridInput.addEventListener("change", (e) => {
+  setGridSize(e)
+})
